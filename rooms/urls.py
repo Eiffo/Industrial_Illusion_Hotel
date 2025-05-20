27ee99a9175ddd1a7879d990b3b242dd437bb201
Eiffo <bastek.xd@o2.pl>
2025-05-20 15:05:56 +0200

    Expand rooms page with detail view

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.rooms, name="rooms"),
+    path("<slug:room_type>/", views.room_detail, name="room_detail")
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import Http404
+from .room_data import ROOM_INFO
 
 def rooms(request):
     return render(request, "rooms/rooms.html")
+
+def room_detail(request, room_type):
+    room = ROOM_INFO
+    if not room:
+        raise Http404("Nie znaleziono takiego pokoju.")
+    return render(request, "rooms/room_detail.html", {"room": room})
