55a22953ac4ef7563bb01a99900fd30fbd500686
Eiffo <bastek.xd@o2.pl>
2025-05-29 12:47:25 +0200

    Complite language switching feature

--- Industrial_Illusion/urls.py
+++ Industrial_Illusion/urls.py
@@ -1,27 +1,11 @@
-"""
-URL configuration for Industrial_Illusion project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
+    path("i18n/", include("django.conf.urls.i18n")),
+    path("admin/", admin.site.urls),
 ]
 
 urlpatterns += i18n_patterns(
@@ -29,5 +13,4 @@ urlpatterns += i18n_patterns(
     path("reservation/", include("reservation.urls")),
     path("rooms/", include("rooms.urls")),
     path("contact/", include("contact.urls")),
-    path("admin/", admin.site.urls),
 )
